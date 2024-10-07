4d7ddeba5f5a49f64cdafd715568864ba801aa71
RubenLasaga <156304653+RubenLasaga@users.noreply.github.com>
2024-10-07 10:52:31 +0200

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Calculadora calculadora1 = new Calculadora(29.0,542.0);
+
+        calculadora1.calcular("+");
+        calculadora1.calcular("-");
+        calculadora1.calcular("*");
+        calculadora1.calcular("/");
+    }
+}
